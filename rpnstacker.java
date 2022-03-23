f3c5f9c66729640ce81bcf6fc814ccc57d8be3cb
Rafael Bernardo Nunes Neto <45106295+RafaelBNN@users.noreply.github.com>
2022-03-23 12:56:44 -0300

    Add files via upload

---
+++ rpnstacker.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.Stack;
+import java.io.*;
+
+public class rpnstacker {
+ 
+    static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) throws Exception
+    {
+
+        File file = new File(
+            "E:/2021.2/Compiladoes/Calc1.stk");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        Stack stk = new Stack();
+
+        String operand1 = "";
+        String operand2 = "";
+
+        while ((st = br.readLine()) != null){
+            if(isNumeric(st)){
+                stk.push((st));
+            }
+            else{
+                operand1 = stk.pop().toString();
+                operand2 = stk.pop().toString();
+
+                switch(st){
+                    case "+": stk.push(String.valueOf(Integer.parseInt(operand1) + Integer.parseInt(operand2))); break;
+                    case "-": stk.push(String.valueOf(Integer.parseInt(operand1) - Integer.parseInt(operand2))); break;
+                    case "*": stk.push(String.valueOf(Integer.parseInt(operand1) * Integer.parseInt(operand2))); break;
+                    case "/": stk.push(String.valueOf(Integer.parseInt(operand1) / Integer.parseInt(operand2))); break;
+                }
+            }
+
+        }
+
+        br.close();
+
+        System.out.println(stk.peek());
+
+    }
+
+}
